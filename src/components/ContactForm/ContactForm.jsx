d6b23051bc16f251dcb0f3749cfb1879676e8607
Hyan18 <HarrisonYan18@gmail.com>
2020-04-29 16:38:20 +0100

    feat: add address details in form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,6 +9,13 @@ class ContactForm extends Component {
             emailAddress: '',
             phoneNumber: '',
             message: '',
+            addressLine1: '',
+            addressLine2: '',
+            cityOrTown: '',
+            stateOrCounty: '',
+            postcode: '',
+            country: '',
+            includeAddressDetails: false,
         }
 
         this.handleChange = this.handleChange.bind(this)
@@ -18,7 +25,7 @@ class ContactForm extends Component {
     handleChange(event) {
         const target = event.target
         const name = target.name
-        const value = target.value
+        const value = target.name === 'includeAddressDetails' ? target.checked : target.value
 
         this.setState({
             [name]: value
@@ -32,21 +39,25 @@ class ContactForm extends Component {
     }
 
     render () {
+        const includeAddressDetails = this.state.includeAddressDetails
+
         return (
             <div className='contact-form-div'>
                 <form onSubmit={this.handleSubmit}>
-                    <div className='nameEmail'>
+                    <div className='inlineInputs'>
                         <label>
                             Full name
                             <input
+                                id="fullName"
                                 name="fullName"
                                 type="text"
                                 value={this.state.fullName}
                                 onChange={this.handleChange} />
                         </label>
-                        <label className='formEmail'>
+                        <label>
                             Email address
                             <input
+                                id="emailAddress"
                                 name="emailAddress"
                                 type="text"
                                 value={this.state.emailAddress}
@@ -71,6 +82,79 @@ class ContactForm extends Component {
                             value={this.state.message}
                             onChange={this.handleChange} />
                     </label>
+                    <div className="inlineInputs">
+                        <input
+                            id="includeAddressDetails"
+                            name="includeAddressDetails"
+                            type="checkbox"
+                            checked={this.state.includeAddressDetails}
+                            onChange={this.handleChange} />
+                        <label id="addAddressDetails">
+                            Add address details
+                        </label>
+                    </div>
+                    {includeAddressDetails &&
+                    <div>
+                        <div className='inlineInputs'>
+                            <label>
+                                Address line 1
+                                <input
+                                id="addressLine1"
+                                name="addressLine1"
+                                type="text"
+                                value={this.state.addressLine1}
+                                onChange={this.handleChange} />
+                            </label>
+                            <label>
+                                Address line 2
+                                <input
+                                id="addressLine2"
+                                name="addressLine2"
+                                type="text"
+                                value={this.state.addressLine2}
+                                onChange={this.handleChange} />
+                            </label>
+                        </div>
+                        <div className='inlineInputs'>
+                            <label>
+                                City/Town
+                                <input
+                                id="cityOrTown"
+                                name="cityOrTown"
+                                type="text"
+                                value={this.state.city}
+                                onChange={this.handleChange} />
+                            </label>
+                            <label>
+                                State/County
+                                <input
+                                id="stateOrCounty"
+                                name="stateOrCounty"
+                                type="text"
+                                value={this.state.stateOrCounty}
+                                onChange={this.handleChange} />
+                            </label>
+                            <label>
+                                Postcode
+                                <input
+                                id="postcode"
+                                name="postcode"
+                                type="text"
+                                value={this.state.postcode}
+                                onChange={this.handleChange} />
+                            </label>
+                            <label>
+                                Country
+                                <input
+                                id="country"
+                                name="country"
+                                type="text"
+                                value={this.state.country}
+                                onChange={this.handleChange} />
+                            </label>
+                        </div>
+                    </div>
+                    }
                     <input type="submit" value="Submit" />
                 </form>
             </div>
