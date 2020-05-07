bc2ce7a3aac29e11eec9da8e906fdd9a10d5a674
Hyan18 <HarrisonYan18@gmail.com>
2020-05-07 15:41:42 +0100

    feat: first bit of info on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import Carousel from '../Carousel/Carousel'
 import './Home.css'
+import officeImg from '../../img/shutterstock_696636415.jpg'
 
 function Home () {
     return (
         <div className='home-div'>
             <Carousel/>
+
+            <div id="info1-container">
+                <div id="info1">
+                    <div id="info1-text">
+                        <p className="info-title">
+                            Fusce urna erat, aliquet et justo nec
+                        </p>
+                        <p className="info-text">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget augue sit amet sem tincidunt viverra ac non quam. Nunc quis dapibus mi. Suspendisse eu risus vulputate sapien elementum tempor nec eget ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut lorem orci, auctor id erat.
+                        </p>
+                        <ul>
+                            <li>
+                                <span>Fusce sed lacus quis mauris feugiat ornare.</span>
+                            </li>
+                            <li>
+                                <span>Quisque feugiat vulputate enim, ac posuere velit sodales venenatis.</span>
+                            </li>
+                            <li>
+                                <span>Quisque ut odio quis augue semper.</span>
+                            </li>
+                            <li>
+                                <span>Maecenas fermentum fringilla ante a luctus.</span>
+                            </li>
+                        </ul>
+                        <button className="dark-blue-button">Learn more</button>
+                    </div>
+
+                    <img id="office-img" src={officeImg}/>
+                </div>
+            </div>
         </div>
     )
 }
