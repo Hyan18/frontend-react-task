cdea9e2f26288bca3518129ee7a8739883146af1
Hyan18 <HarrisonYan18@gmail.com>
2020-05-08 12:04:02 +0100

    feat: info2 content

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home () {
                         <p className="info-title">
                             Fusce urna erat, aliquet et justo nec
                         </p>
-                        <p className="info-text">
+                        <p className="info-subtext">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget augue sit amet sem tincidunt viverra ac non quam. Nunc quis dapibus mi. Suspendisse eu risus vulputate sapien elementum tempor nec eget ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut lorem orci, auctor id erat.
                         </p>
                         <ul>
@@ -35,6 +35,22 @@ function Home () {
                     </div>
 
                     <img id="office-img" src={officeImg}/>
+                </div>
+            </div>
+
+            <div id="info2-container">
+                <div id="info2">
+                    <div id="info2-text-container">
+                        <div id="info2-text" >
+                            <p id="info2-title" className="info-title">
+                                Proin leo purus, volutpat id metus sit amet, dapibus iaculis turpis.
+                            </p>
+                            <p id="info2-subtext" className="info-subtext">
+                            Sed pharetra egestas purus, id ornare dolor suscipit ut. Morbi consequat fringilla eros vitae pharetra. Quisque pellentesque varius urna. Nunc quis nulla ultricies, facilisis orci non, iaculis ligula. Mauris quis felis consectetur, commodo ipsum eget, euismod odio. Sed finibus sagittis elit, sit amet tincidunt massa commodo non. Nullam dui nunc, dignissim sit amet lectus pulvinar, elementum interdum tellus.
+                            </p>
+                            <button id="info2-login">Log in</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
